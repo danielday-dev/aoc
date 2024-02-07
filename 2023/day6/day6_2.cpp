9a9a548b825b021d8094e76bf71254a9e4ff3e44
danielday-dev <141579840+danielday-dev@users.noreply.github.com>
2024-02-07 00:12:16 +0000

    completed day 6 part 2 it was long long

---
+++ 2023/day6/day6_2.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+// go farther than the furthest recorded distance in each race
+// time is a constraint
+// Charge time vs time going forward
+
+//  input is race time and distance to beat
+
+//  starting speed is 0 mm/ms
+//  each millisecond spent holding down adds 1 to speed
+
+// Determine the number of ways you could beat the record in each race.
+// What do you get if you multiply these numbers together?
+
+long long examineRace(long long, long long);
+
+int main(){
+    ifstream myfile;
+    myfile.open("input.txt");
+    string line;
+    string dump;
+
+    getline(myfile, line);
+    stringstream ssLine1(line);
+
+    getline(myfile, line);
+    stringstream ssLine2(line);
+
+    int numInputs = 1;
+
+    long long times[numInputs];
+    long long distances[numInputs];
+    long long possible_results[numInputs];
+    long long results = 1;
+
+    ssLine1 >> dump;
+    ssLine2 >> dump;
+    for(int i = 0; i < numInputs; i++){
+        ssLine1 >> times[i];
+        ssLine2 >> distances[i];
+        possible_results[i] = examineRace(times[i], distances[i]);
+        results *= possible_results[i];
+    }
+
+    printf ("%i", results);
+
+    return EXIT_SUCCESS;
+
+}
+
+/**
+ * @return number of possible ways to win race
+*/
+long long examineRace(long long maxTime, long long goalDistance){
+    //find min time
+    //find max time
+    //return max - min
+
+    //iterate through entire race time until end time is found
+    // find min and max times mathematically
+
+    long long possibleValues;
+    long long minimum;
+    bool minFound = false;
+    for(long long speed = 0; speed <= maxTime; speed++){
+
+        long long boatDistance = (maxTime - speed) * speed;
+        if (speed % 100000 == 0){
+            printf("%i\n", boatDistance);
+        }
+        if (!minFound && (boatDistance > goalDistance)){
+            // get max
+            minFound = true;
+            minimum = speed;
+        }
+        if( minFound && ((boatDistance <= goalDistance))){
+            // get min
+            possibleValues = speed - minimum;
+            return possibleValues;
+        }
+    }
+    return -1;
+}
